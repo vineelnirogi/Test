d0adc086d3fabde8fe906e633d52306856273a82
vineel <vnirogi@gmail.com>
2015-10-24 14:19:41 +0530

    commit 5

---
+++ SampleTest1/src/Test1/AddNumbers.java
@@ -0,0 +1,14 @@
+package Test1;
+
+public class AddNumbers {
+
+	/**
+	 * @param args
+	 */
+	public static int main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 2 + 3;
+		return sum;
+	}
+
+}
